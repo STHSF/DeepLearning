82643492c66728a8842dbacb5a31745e221ea16e
STHSF <woailiyu@163.com>
2016-10-19 22:00:09 +0800

    remove datasets

--- TF/RNN/rnn_classify.py
+++ TF/RNN/rnn_classify.py
@@ -37,19 +37,19 @@ biases = {
 }
 
 
-def RNN(X, weights, biases):
+def rnn(input_data, weights, biases):
     # hidden layer for input to cell
     ########################################
 
     # transpose the inputs shape from
     # X ==> (128 batch * 28 steps, 28 inputs)
-    X = tf.reshape(X, [-1, n_inputs])
+    input_data = tf.reshape(input_data, [-1, n_inputs])
 
     # into hidden
-    # X_in = (128 batch * 28 steps, 128 hidden)
-    X_in = tf.matmul(X, weights['in']) + biases['in']
-    # X_in ==> (128 batch, 28 steps, 128 hidden)
-    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+    # data_in = (128 batch * 28 steps, 128 hidden)
+    data_in = tf.matmul(input_data, weights['in']) + biases['in']
+    # data_in ==> (128 batch, 28 steps, 128 hidden)
+    data_in = tf.reshape(data_in, [-1, n_steps, n_hidden_units])
 
     # cell
     ##########################################
@@ -62,12 +62,12 @@ def RNN(X, weights, biases):
     # You have 2 options for following step.
     # 1: tf.nn.rnn(cell, inputs);
     # 2: tf.nn.dynamic_rnn(cell, inputs).
-    # If use option 1, you have to modified the shape of X_in, go and check out this:
+    # If use option 1, you have to modified the shape of data_in, go and check out this:
     # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
     # In here, we go for option 2.
-    # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as X_in.
+    # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as data_in.
     # Make sure the time_major is changed accordingly.
-    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
+    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, data_in, initial_state=_init_state, time_major=False)
 
     # hidden layer for output as the final results
     #############################################
@@ -81,27 +81,22 @@ def RNN(X, weights, biases):
     return results
 
 
-pred = RNN(x, weights, biases)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+predict = rnn(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predict, y))
 train = tf.train.AdamOptimizer(lr).minimize(cost)
 
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+correct_predict = tf.equal(tf.argmax(predict, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
 
 init = tf.initialize_all_variables()
+
 with tf.Session() as sess:
     sess.run(init)
     step = 0
     while step * batch_size < training_iters:
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
         batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
-        sess.run([train], feed_dict={
-            x: batch_xs,
-            y: batch_ys,
-        })
+        sess.run([train], feed_dict={x: batch_xs, y: batch_ys, })
         if step % 20 == 0:
-            print(sess.run(accuracy, feed_dict={
-            x: batch_xs,
-            y: batch_ys,
-        }))
+            print(sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys, }))
         step += 1
